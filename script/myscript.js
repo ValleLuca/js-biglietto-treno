5799d918e13396a9b2e3eaa2d0afdfc535b49521
ValleLuca <valle.lucaAb@outlook.it>
2021-12-03 17:06:40 +0100

    finale

--- script/myscript.js
+++ script/myscript.js
@@ -3,9 +3,15 @@ let kmValue = prompt('Inserisci i kilometri che vuoi percorrere');
 let ageValue = prompt('Inserisci i tuoi anni');
 
 const priceValue = kmValue * 0.21;
-console.log(priceValue)
 
 if (ageValue < 18 ){
     const scontominorenniValue = priceValue - ((priceValue / 100) * 20);
-    console.log(scontominorenniValue)
+    console.log(scontominorenniValue);
+    document.getElementById('finalprice').innerHTML = scontominorenniValue.toFixed(2);
+}else if(ageValue >= 65){
+    const scontoanzianiValue = priceValue - ((priceValue / 100) * 40);
+    console.log(scontoanzianiValue);
+    document.getElementById('finalprice').innerHTML = scontoanzianiValue.toFixed(2);
+}else{
+    document.getElementById('finalprice').innerHTML = priceValue.toFixed(2);
 }
